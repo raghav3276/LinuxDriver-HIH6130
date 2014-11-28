1e32042917e7173213653e2c6bc9c8992dff1a4e
A Raghavendra Rao <arrao@cdac.in>
2014-11-28 10:53:00 +0530

    I2C data read functionality added
    So far the driver could read the data from the sensor and store
    it into the device specific structure

--- KERN_SRC/hih6130.c
+++ KERN_SRC/hih6130.c
@@ -2,12 +2,9 @@
 #include <linux/module.h>
 #include <linux/i2c.h>
 #include <linux/delay.h>
-#include <linux/debugfs.h>
-#include <linux/uaccess.h>
 
 struct hih6130_dev {
 	struct i2c_client *client;
-	struct dentry *debugfs;
 
 	int humidity;
 	int temperature;
@@ -71,33 +68,9 @@ out:
 	return ret;
 }
 
-static ssize_t hih6130_read(struct file *file, char __user *ubuff,
-		size_t size, loff_t *loff)
-{
-	int ret;
-	char kbuff[100];
-	struct hih6130_dev *dev = file->f_inode->i_private;
-
-	ret = hih6130_update_data(dev);
-	if (ret < 0)
-		return ret;
-
-	memset(kbuff, 0, sizeof(kbuff));
-	sprintf(kbuff, "Humidity : %d\nTemperature : %d\n",
-					dev->humidity, dev->temperature);
-
-	return simple_read_from_buffer(ubuff, size, loff,
-					kbuff, strlen(kbuff) + 1);
-}
-
-static const struct file_operations hih6102_fops = {
-		.read = hih6130_read
-};
-
 static int hih6130_probe(struct i2c_client *client,
 		const struct i2c_device_id *i2c_device_id)
 {
-	int ret;
 	struct hih6130_dev *dev;
 
 	dev = devm_kzalloc(&client->dev, sizeof(*dev), GFP_KERNEL);
@@ -108,31 +81,8 @@ static int hih6130_probe(struct i2c_client *client,
 
 	mutex_init(&dev->lock);
 
-	dev->debugfs = debugfs_create_file("hih6130", S_IRUGO, NULL,
-					dev, &hih6102_fops);
-	if (!dev->debugfs) {
-		ret = -EFAULT;
-		goto debugfs_fail;
-	}
-
 	i2c_set_clientdata(client, dev);
 
-	dev_info(&client->dev, "device probed\n");
-	return 0;
-
-debugfs_fail:
-	devm_kfree(&client->dev, dev);
-
-	return ret;
-}
-
-static int hih6130_remove(struct i2c_client *client)
-{
-	struct hih6130_dev *dev = i2c_get_clientdata(client);
-
-	debugfs_remove(dev->debugfs);
-
-	dev_info(&client->dev, "device removed\n");
 	return 0;
 }
 
@@ -147,7 +97,6 @@ static struct i2c_driver hih6130_driver = {
 		},
 		.id_table = hih6130_id,
 		.probe = hih6130_probe,
-		.remove = hih6130_remove
 };
 
 module_i2c_driver(hih6130_driver);
